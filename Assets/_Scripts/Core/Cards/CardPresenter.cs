c6c577ef9a85fc98eef2f14bb6b064ecafc0da34
oskar <oskar.vistorskis@gmail.com>
2026-02-01 12:38:29 +0200

    Preparation before build

--- Assets/_Scripts/Core/Cards/CardPresenter.cs
+++ Assets/_Scripts/Core/Cards/CardPresenter.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMPro;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
