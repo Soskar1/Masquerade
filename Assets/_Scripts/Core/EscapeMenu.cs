6244a51591fce22401c31f695f029c268b956832
oskar <oskar.vistorskis@gmail.com>
2026-02-01 13:59:22 +0200

    Added resetadfsla;gk button

--- Assets/_Scripts/Core/EscapeMenu.cs
+++ Assets/_Scripts/Core/EscapeMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EscapeMenu : MonoBehaviour
 {
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
